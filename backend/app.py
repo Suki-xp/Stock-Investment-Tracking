c3acf52c49773e9e682580a595719ad650f92feb
Ahmed Joyia <ahmedjoyia36@gmail.com>
2026-01-06 15:44:50 -0500

    API verification
    Code to make sure that the API/server hosting the program can actually run
    
    (It uses the http://localhost:5000/api/health) to verify

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,37 @@
 #Holds the backend code that will run the stock data, live tracking, 
 #graphing plotting/tracking etc
-from flask import Flask, requst, jsonify 
+from flask import Flask, request, jsonify 
 from flask_cors import CORS
-from dateTime import datetime
+from datetime import datetime
 import yfinance as ytrack
 import pandas as pd
 
 #This is to allow for the code to also run the front end section of it
-app = Flask(__name___)
+app = Flask(__name__)
 CORS(app)
 
 #This will act as the temp memory that stores the users input data to track
+userPortfolios = {}
+userTranscations = []
+
+#Need to verify that the health of the API is well
+@app.route('/api/health', methods = ['GET'])
+def health_check():
+    try:
+        response = {
+            "status": "healthy",
+            "service": "functional",
+            "upTime": "GOOD"
+        }
+        return jsonify(response), 200
+    except Exception as error:
+        response = {
+            "status": "Unhealthy",
+            "service": "Non-functional",
+            "upTime": "TERRIBLE",
+            "error": error
+        }
+        return jsonify(response), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
