a41ef3e274df962dd83371341e313c6a22c3d1b5
Ahmed Joyia <ahmedjoyia36@gmail.com>
2026-01-06 19:41:26 -0500

    Getting stock_info
    Added a method that will get all the necessary information tied to the stock for the user.

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, jsonify 
 from flask_cors import CORS
 from datetime import datetime
-import yfinance as ytrack
+import yfinance as yStock
 import pandas as pd
 
 #This is to allow for the code to also run the front end section of it
@@ -36,10 +36,35 @@ def health_check():
 #This method is responsible for getting the stock information 
 #via the yFinance and add to the sturcture array of the user
 #With its goal being able to determine if its userful enough or not
-@app.route('/api/stock/<ticker>', methods= ['GET'])
-def proper_stock_info():
+def proper_stock_info(ticker):
+    #Working with the try and except format to retrieve the data
+    #this makes it that onces the user visits the /api/stock/"" that 
+    #will try and retrieve that stock info
+    
+    try:
+        find_stock = yStock.Ticker(ticker)
+        stock_info = find_stock.info #Grabs relavent info about the stock
 
-    #First thing we got to do now is creating the endpoint so we can get the stock data
+        #Need to check that the stock isnt empty and has a current price ready
+        stock_price = stock_info.get('currentPrice') or stock_info.get('regularMarketPrice', 0)
+        if stock_price is 0:
+            return None
+        
+        #Make sure stock is relevant enough to find all the info on
+        if (len(stock_info) > 5):
+            #Then add it to the dictionary which gets the tags to it
+            stock_data = {
+                'symbol': stock_info.get('symbol', ticker.upper()),
+                'longName': stock_info.get('longName', 'unknown'),
+                'currentPrice': stock_price,
+                'marketCap': stock_info.get('marketCap', 0),
+                'sector': stock_info.get('sector', 'Unknown'), 
+                'industry': stock_info.get('industry', 'Unknown'),
+                'currency': stock_info.get('currency', 'USD')
+            }
+            return stock_data
+    except Exception as e:
+        return {'error': str(e), 'ticker': ticker} #means the stock couldn't be located
 
 if __name__ == "__main__":
     proper_stock_info()
